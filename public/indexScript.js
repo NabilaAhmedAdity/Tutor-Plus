41522b810edd7df5e173518166ac6cfd8dfa577d
nabila <nabilaahmed.adity@gmail.com>
2017-07-16 01:31:57 +0600

    Gave a new look to index page

--- public/indexScript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("a#trigger").hover(function(){
-    	$(this).parents("#external-card").children("div#pop-up").show();
-        $(this).parents("#external-card").css("background-color", "#FFFF99");
-        }, function(){
-        $(this).parents("#external-card").children("div#pop-up").hide();
-        $(this).parents("#external-card").css("background-color", "white");
-    });
-});
